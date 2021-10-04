fa43634c9012a0392e41c6c0d0decc20f5caa41a
km2021 <meng.kang@taobit.cn>
2021-10-04 21:45:19 +0800

    fix:打包

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-yarn build:stage
+# yarn build:stage
+###
+ # @Author: km2021
+ # @Date: 2020-11-16 23:14:39
+ # @LastEditTime: 2021-10-04 21:41:25
+ # @Description: 
+ # @FilePath: /sell-web/build.sh
+ # 
+### 
 
 yarn build:prod
